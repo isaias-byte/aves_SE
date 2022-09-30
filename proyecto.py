62be137145c91bdd5a9ddb26d2d5306db032680c
Servicio Social <serviciosocial@cuceimobiles-Mac-mini.local>
2022-09-30 11:43:29 -0500

    Evaluacion del resultado: no fue encontrado, fue encontrado (mostrar características), hay mas de una ave con las mismas características (mostrar aves). Ahora es posible omitir preguntas escribiendo una x

--- proyecto.py
+++ proyecto.py
@@ -4,10 +4,10 @@ from aves import aves
 # loras: negras (example
 # nuca: naranja (example)
 # auriculares: grises
-#alas: rojas
-#cola: roja
+# alas: rojas
+# cola: roja
 # patas: amarillas
-#rabadilla: amarilla
+# rabadilla: amarilla
 
 print('\n')
 print('------------------------------------------')
@@ -15,12 +15,14 @@ print('---------- CLASIFICADOR DE AVES ----------')
 print('------------------------------------------')
 print('\n')
 
+print('A continuación se te realizarán una serie de preguntas,')
+print('en caso de no saber la respuesta puedes responder con una "x"\n')
 
-print('Preguntas')
-ojos = input('Color de ojos: ')
-pico = input('Color de pico: ')
-alas = input('Color de alas: ')
-cola = input('Color de cola: ')
+print('\nPreguntas:')
+ojos = input('Color de ojos: ').lower()
+pico = input('Color de pico: ').lower()
+alas = input('Color de alas: ').lower()
+cola = input('Color de cola: ').lower()
 
 preguntas = {
     'ojos': ojos,
@@ -39,24 +41,35 @@ preguntas = {
 #     print('\n')
 
 # Mostrar todo lo que capturó el usuario
-print('\n')
+print('\n\nTu búsqueda fue la siguiente:')
 for clave, valor in preguntas.items():
     print(clave, ':', valor)
-
 print('\n')
 
-avesResultado = dict()
+avesResultado = list()
 
 for ave, contenido in aves.items():
     if (
-        (preguntas.get('ojos') in contenido.get('ojos')) and
-        (preguntas.get('pico') in contenido.get('pico')) and
-        (preguntas.get('alas') in contenido.get('alas')) and
-        (preguntas.get('cola') in contenido.get('cola'))
+        ((preguntas.get('ojos') in contenido.get('ojos')) or (preguntas.get('ojos') == 'x')) and
+        ((preguntas.get('pico') in contenido.get('pico')) or (preguntas.get('pico') == 'x')) and
+        ((preguntas.get('alas') in contenido.get('alas')) or (preguntas.get('alas') == 'x')) and
+        ((preguntas.get('cola') in contenido.get('cola')) or (preguntas.get('cola') == 'x'))
         ):
-            print('------------------------------')
-            print(ave, ':')
+            avesResultado.append(ave)
+    
+if(len(avesResultado) == 1):
+    for ave, contenido in aves.items():
+        if(avesResultado[0] == ave):
+            print('La ave que buscas es:', ave)
+            print('\nEsta ave tiene:\n')
             for caracteristica, valor in contenido.items():
-                print(caracteristica, ':', valor, ',', len(valor))
-            print('------------------------------')
+                print(caracteristica, ':', valor)
             print('\n')
+            break
+elif(len(avesResultado) == 0):
+    print('No pudimos encontrar el ave que buscas :(\n')
+elif(len(avesResultado) > 1):
+    print('La ave que buscas puede ser una de estas:\n')
+    for ave in avesResultado:
+        print(ave)
+    print('\n')
