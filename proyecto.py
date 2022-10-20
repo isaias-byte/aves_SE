7169e4892780d6a430a5891c81a38d43fc111142
Jesús Ricardo Delgado Sánchez <chuyatlas2001@hotmail.com>
2022-10-19 21:40:00 -0500

    Descripción del ave cuando es encontrada y evaluación de nuevas características

--- proyecto.py
+++ proyecto.py
@@ -16,6 +16,10 @@ alas = input('Color de alas: ').lower()
 cola = input('Color de cola: ').lower()
 espalda = input('Color de espalda: ').lower()
 pecho = input('Color del pecho: ').lower()
+patas = ''
+cabeza = ''
+cuerpo = ''
+frente = ''
 
 preguntas = {
     'ojos': ojos,
@@ -24,6 +28,10 @@ preguntas = {
     'cola': cola,
     'espalda': espalda,
     'pecho': pecho,
+    'patas': patas,
+    'cabeza': cabeza,
+    'cuerpo': cuerpo,
+    'frente': frente,
 }
 
 # Mostrar todas las aves
@@ -46,12 +54,16 @@ avesResultado = list()
 # Búsqueda del ave
 for ave, contenido in aves.items():
     if (
-        ((preguntas.get('ojos') in contenido.get('ojos')) or (preguntas.get('ojos') == 'x')) and
-        ((preguntas.get('pico') in contenido.get('pico')) or (preguntas.get('pico') == 'x')) and
-        ((preguntas.get('alas') in contenido.get('alas')) or (preguntas.get('alas') == 'x')) and
-        ((preguntas.get('cola') in contenido.get('cola')) or (preguntas.get('cola') == 'x')) and
-        ((preguntas.get('espalda') in contenido.get('espalda')) or (preguntas.get('espalda') == 'x')) and
-        ((preguntas.get('pecho') in contenido.get('pecho')) or (preguntas.get('pecho') == 'x'))
+        ((preguntas.get('ojos') in contenido.get('ojos')) or (preguntas.get('ojos') == 'x') or (preguntas.get('ojos') == '')) and
+        ((preguntas.get('pico') in contenido.get('pico')) or (preguntas.get('pico') == 'x') or (preguntas.get('pico') == '')) and
+        ((preguntas.get('alas') in contenido.get('alas')) or (preguntas.get('alas') == 'x') or (preguntas.get('alas') == '')) and
+        ((preguntas.get('cola') in contenido.get('cola')) or (preguntas.get('cola') == 'x') or (preguntas.get('cola') == '')) and
+        ((preguntas.get('espalda') in contenido.get('espalda')) or (preguntas.get('espalda') == 'x') or (preguntas.get('espalda') == '')) and
+        ((preguntas.get('pecho') in contenido.get('pecho')) or (preguntas.get('pecho') == 'x') or (preguntas.get('pecho') == '')) and
+        ((preguntas.get('espalda') in contenido.get('patas')) or (preguntas.get('patas') == 'x') or (preguntas.get('patas') == '')) and
+        ((preguntas.get('espalda') in contenido.get('cabeza')) or (preguntas.get('cabeza') == 'x') or (preguntas.get('cabeza') == '')) and
+        ((preguntas.get('espalda') in contenido.get('cuerpo')) or (preguntas.get('cuerpo') == 'x') or (preguntas.get('cuerpo') == '')) and
+        ((preguntas.get('espalda') in contenido.get('frente')) or (preguntas.get('frente') == 'x') or (preguntas.get('frente') == ''))
         ):
             avesResultado.append(ave)
 
@@ -59,12 +71,15 @@ for ave, contenido in aves.items():
 if(len(avesResultado) == 1):   # Ave encontrada
     for ave, contenido in aves.items():
         if(avesResultado[0] == ave):
+            ave = ave.replace('_', ' ').title()
             print('La ave que buscas es:', ave)
-            print('\nEsta ave tiene:\n')
+            descripcion = 'El ave ' + ave + ' se caracteriza por tener '
             for caracteristica, valor in contenido.items():
-                if(preguntas.__contains__(caracteristica) and preguntas.get(caracteristica) != 'x'):
-                    print(caracteristica, ':', ', '.join(valor))
-            print('\n')
+                if(preguntas.__contains__(caracteristica) and preguntas.get(caracteristica) != 'x' and preguntas.get(caracteristica) != ''):
+                    descripcion += caracteristica + ' color ' + ', '.join(valor) + ', '
+            descripcion = descripcion[:-2]
+            descripcion += '.'
+            print(descripcion)
             break
 elif(len(avesResultado) == 0):   # No se encontraron coincidencias
     print('No pudimos encontrar el ave que buscas :(\n')
